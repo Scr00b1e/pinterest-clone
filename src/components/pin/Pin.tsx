ba9442456cb16e4e5cd291c9fd5565b026f26418
Scr00b1e <kbolot312@gmail.com>
2022-09-25 21:37:08 +0600

    pinterest clone

--- src/components/pin/Pin.tsx
+++ src/components/pin/Pin.tsx
@@ -8,14 +8,16 @@ type PinProps = {
 
 const Pin: React.FC<PinProps> = ({type, img}) => {
   return (
-    <>
       <div className={type}>
-          <img src={img} alt="" />
+          <img src={img} alt="" className='pin__img'/>
+          <p className="pin__description">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. A reprehenderit obcaecati iste praesentium cum vero maiores quos soluta, repudiandae et, vitae delectus! Quisquam earum perspiciatis minima non facilis aliquid voluptatibus.
+          </p>
+          <div className="pin__author">
+            <img src="https://via.placeholder.com/50" alt="" className='pin__icon'/>
+            <h1 className='pin__name'>Just Jack</h1>
+          </div>
       </div>
-      <div className="pin__author">
-        <img src="https://via.placeholder.com/50" alt="" className='pin__icon'/>
-      </div>
-    </>
   )
 }
 
